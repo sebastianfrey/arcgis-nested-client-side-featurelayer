37a3143b08ffb990b68da2e132d92dd52b7af691
Sebastian Frey <sebastian.frey@outlook.com>
2024-07-30 14:32:33 +0200

    chore: progress

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
+import "@esri/calcite-components/dist/calcite/calcite.css";
+import "@arcgis/core/assets/esri/themes/light/main.css";
+
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
 import GroupLayer from "@arcgis/core/layers/GroupLayer";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import LayerList from "@arcgis/core/widgets/LayerList";
+import * as reactiveUtils from "@arcgis/core/core/reactiveUtils";
+
+import { setAssetPath } from "@esri/calcite-components/dist/components";
+
+import "@esri/calcite-components/dist/components/calcite-shell";
+import "@esri/calcite-components/dist/components/calcite-shell-panel";
+import "@esri/calcite-components/dist/components/calcite-navigation";
+import "@esri/calcite-components/dist/components/calcite-navigation-logo";
+import "@esri/calcite-components/dist/components/calcite-action";
+import "@esri/calcite-components/dist/components/calcite-action-bar";
+import "@esri/calcite-components/dist/components/calcite-panel";
+import "@esri/calcite-components/dist/components/calcite-button";
+import "@esri/calcite-components/dist/components/calcite-icon";
+import "@esri/calcite-components/dist/components/calcite-slider";
+import "@esri/calcite-components/dist/components/calcite-label";
 
 import "./style.css";
+
+setAssetPath("/assets");
 
 const map = new Map({
   basemap: "streets",
@@ -13,121 +33,186 @@ const map = new Map({
 const view = new MapView({
   container: "viewDiv",
   map,
+  padding: {
+    left: 49,
+  },
 });
 
 const layerList = new LayerList({
-  view
+  view,
+  visibilityAppearance: "checkbox",
+  container: document.getElementById("layers-container")!,
 });
-view.ui.add(layerList, {
-  position: "top-left"
-});
 
-interface ProjectVariantItem {
-  id: number;
-  title: string;
-}
-
-interface ProjectItem {
-  title: string;
-  variants: ProjectVariantItem[];
-}
-
-const createWindFarmLayers = () => {
-  const wtgLayer = new GroupLayer({
-    title: 'WTG',
+const createGroupLayerWithFeatureLayers = (
+  title: string,
+  childLayers: string[]
+) => {
+  const groupLayer = new GroupLayer({
+    title,
   });
-  const wtgPointLayer = new FeatureLayer({
-    source: [],
-    title: "Center point",
-    objectIdField: "ID",
-    spatialReference: view.spatialReference,
-    geometryType: "point",
-    fields: [
-      {
-        name: "ID",
-        alias: "ID",
-        type: "oid",
-      },
-    ],
+  const sublayers = childLayers.map((title) => {
+    return new FeatureLayer({
+      source: [],
+      title,
+      objectIdField: "ID",
+      spatialReference: view.spatialReference,
+      geometryType: "polygon",
+      fields: [
+        {
+          name: "ID",
+          alias: "ID",
+          type: "oid",
+        },
+      ],
+    });
   });
-  const wtgFoundationLayer = new FeatureLayer({
-    source: [],
-    title: "Foundation area",
-    objectIdField: "ID",
-    spatialReference: view.spatialReference,
-    geometryType: "polygon",
-    fields: [
-      {
-        name: "ID",
-        alias: "ID",
-        type: "oid",
-      },
-    ],
-  });
-  const wtgRotationLayer = new FeatureLayer({
-    source: [],
-    title: "Rotation area",
-    objectIdField: "ID",
-    spatialReference: view.spatialReference,
-    geometryType: "polygon",
-    fields: [
-      {
-        name: "ID",
-        alias: "ID",
-        type: "oid",
-      },
-    ],
-  });
-  wtgLayer.addMany([wtgRotationLayer, wtgFoundationLayer, wtgPointLayer]);
-
-  const cableLayer = new GroupLayer({
-    title: 'Cable',
-  });
-  const cableCorridorLayer = new FeatureLayer({
-    source: [],
-    title: "Corridor",
-    objectIdField: "ID",
-    spatialReference: view.spatialReference,
-    geometryType: "polyline",
-    fields: [
-      {
-        name: "ID",
-        alias: "ID",
-        type: "oid",
-      },
-    ],
-  });
-  cableLayer.add(cableCorridorLayer);
-
-  return [wtgLayer, cableLayer];
+  groupLayer.addMany(sublayers);
+  return groupLayer;
 };
 
-const addProjectToMap = (project: ProjectItem) => {
+const createWindFarmLayers = () => {
+  const wtgLayer = createGroupLayerWithFeatureLayers("WTG", [
+    "WEA-Position",
+    "Umfahrung",
+    "Fundament",
+    "Fundamentschutz",
+    "Permanente Inanspruchnahme",
+    "Temporäre Inanspruchname",
+    "Kranstellfläche",
+    "Kranauslegerfläche",
+    "Lagerflächen",
+    "Böschungsring",
+    "Rotorüberstreichfläche",
+    "Reduzierte Abstandsfläche",
+    "Abstandsfläche",
+    "Turbulenzellipse (techn. Abstand)",
+    "Turbulenzkreis 2.5 D",
+    "Turbulenzkreis 3 D",
+    "Turbulenzkreis 3.5 D",
+  ]);
+  const wtsLayer = createGroupLayerWithFeatureLayers("WTS", [
+    "WEA-Position",
+    "Umfahrung",
+    "Fundament",
+    "Fundamentschutz",
+    "Permanente Inanspruchnahme",
+    "Temporäre Inanspruchname",
+  ]);
+
+  const accessLayer = createGroupLayerWithFeatureLayers("Zuwegung", [
+    "Mittellinie Zuwegung",
+    "Schutzstreifen",
+    "Serviceweg",
+    "Überstreichfläche (Schleppkurve)",
+    "Permanente Zuwegung",
+    "Temporäre Zuwegung",
+  ]);
+  const claimLayer = createGroupLayerWithFeatureLayers("Inanspruchnahme", [
+    "Permanente Inanspruchnahme",
+    "Temporäre Inanspruchname",
+    "Inanspruchnahme",
+  ]);
+
+  const cableLayer = createGroupLayerWithFeatureLayers("Kabel", ["Kabel"]);
+
+  return [wtgLayer, wtsLayer, accessLayer, claimLayer, cableLayer];
+};
+
+const addProjectToMap = (title: string) => {
   const rootLayer = new GroupLayer({
-    title: project.title,
+    title,
   });
 
-  for (const variant of project.variants) {
+  map.add(rootLayer);
+
+  return rootLayer;
+};
+
+const updateVariants = (rootLayer: GroupLayer, groupCount: number) => {
+  rootLayer.removeAll();
+  for (let i = 0; i < groupCount; i++) {
     const variantLayer = new GroupLayer({
-      title: variant.title,
+      title: `Variant ${i + 1}`,
     });
     variantLayer.addMany(createWindFarmLayers());
-
     rootLayer.add(variantLayer);
   }
 
-  map.add(rootLayer);
+  document.getElementById("layer-count")!.innerHTML = `Number of layers ${rootLayer.allLayers.length}`;
 };
 
+let activeWidget: any = null;
+
 view.when(() => {
-  addProjectToMap({
-    title: "My Project",
-    variants: [
-      { id: 1, title: "V001" },
-      { id: 2, title: "V002" },
-      { id: 3, title: "V003" },
-      { id: 4, title: "V004" },
-    ],
+  (document.querySelector("#header-title")! as any).heading =
+    "More than 200 nested client-side FeatureLayers cause significant frame drops";
+  const handleActionBarClick = ({ target }: any) => {
+    if (target.tagName !== "CALCITE-ACTION") {
+      return;
+    }
+
+    if (activeWidget) {
+      (
+        document.querySelector(`[data-action-id=${activeWidget}]`)! as any
+      ).active = false;
+      (
+        document.querySelector(`[data-panel-id=${activeWidget}]`)! as any
+      ).hidden = true;
+    }
+
+    const nextWidget = target.dataset.actionId;
+    if (nextWidget !== activeWidget) {
+      (
+        document.querySelector(`[data-action-id=${nextWidget}]`)! as any
+      ).active = true;
+      (document.querySelector(`[data-panel-id=${nextWidget}]`)! as any).hidden =
+        false;
+      activeWidget = nextWidget;
+    } else {
+      activeWidget = null;
+    }
+  };
+
+  document
+    .querySelector("calcite-action-bar")!
+    .addEventListener("click", handleActionBarClick);
+
+  let actionBarExpanded = false;
+
+  document.addEventListener("calciteActionBarToggle", () => {
+    actionBarExpanded = !actionBarExpanded;
+    view.padding = {
+      left: actionBarExpanded ? 150 : 49,
+    };
+  });
+
+  document.querySelector("calcite-shell")!.hidden = false;
+
+  const rootLayer = addProjectToMap("Root Group");
+
+  const variantsSlider = document.querySelector("calcite-slider")!;
+  variantsSlider!.addEventListener("calciteSliderChange", () => {
+    updateVariants(rootLayer, variantsSlider!.value as number);
+  });
+  updateVariants(rootLayer, variantsSlider!.value as number);
+
+  view.whenLayerView(rootLayer).then(() => {
+    reactiveUtils
+      .whenOnce(
+        () =>
+          document
+            .getElementById("layers-container")
+            ?.querySelector("calcite-list-item") !== null
+      )
+      .then(() => {
+        document
+          .getElementById("layers-container")!
+          .querySelector("calcite-list-item")!.open = true;
+      });
   });
 });
 
+(window as any).map = map;
+(window as any).view = view;
+(window as any).layerList = layerList;
